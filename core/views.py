ac0699881bd9f5f7b2b494962cf04f5b6c811a20
ilton <xlsonplay@gmail.com>
2022-11-10 19:39:05 -0300

    Criando tela de listagem de eventos, e redirecionamento a pagina principal

--- core/views.py
+++ core/views.py
@@ -3,12 +3,15 @@ from core.models import Evento
 # Create your views here.
 from core.models import Evento
 
+def index(request):
+    redirect('/agenda/')
+
 def lista_eventos(request):
     #criar um request no banco para usuario
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario) #filtrar as informações para aparecer apenas a do usuario
-    response = {'eventos': evento}
-    return render(request,'agenda.html', response)
+    dados = {'eventos': evento}
+    return render(request,'agenda.html', dados)
 
 
 # def index(request):
